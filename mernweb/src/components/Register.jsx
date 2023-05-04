7b11eee081f1e082c82bcf71f58f9a3b73c1e1ae
Akshay3875 <c.harikumar3875@gmail.com>
2023-05-04 16:15:43 +0530

    hh

--- mernweb/src/components/Register.jsx
+++ mernweb/src/components/Register.jsx
@@ -17,24 +17,24 @@ const Register = () => {
                     <div className="col-md-6 p-5">
                         <h1 className="display-6 fw-bolder mb-5">REGISTER</h1>
                         <form>
-                        <div class="mb-3">
+                        <div className="mb-3">
                                 <label for="name" lass="form-label">Username</label>
-                                <input type="text" class="form-control" id="name"/>
+                                <input type="text" className="form-control" id="name"/>
                             </div>
-                            <div class="mb-3">
-                                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                            <div className="mb-3">
+                                <label for="exampleInputEmail1" className="form-label">Email address</label>
+                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                             </div>
-                            <div class="mb-3">
-                                <label for="exampleInputPassword1" class="form-label">Password</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1" />
+                            <div className="mb-3">
+                                <label for="exampleInputPassword1" className="form-label">Password</label>
+                                <input type="password" className="form-control" id="exampleInputPassword1" />
                             </div>
-                            <div class="mb-3 form-check">
-                                <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                            <div className="mb-3 form-check">
+                                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                                <label className="form-check-label" for="exampleCheck1">I Agree Terms And Conditions</label>
                             </div>
-                            <button type="submit" class="btn btn-primary">Submit</button>
+                            <button type="submit" className="btn btn-outline-primary w-100 mt-4 rounded-pill">Register</button>
                         </form>
                     </div>
                 </div>
