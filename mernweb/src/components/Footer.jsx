1c1b69f29cf7c8c4abfa6644d996fa8de23d15a3
Developer <ijasahmediku987@gmail.com>
2023-05-31 16:47:15 +0530

    footerFix

--- mernweb/src/components/Footer.jsx
+++ mernweb/src/components/Footer.jsx
@@ -1,115 +1,114 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faLinkedin, faFacebook } from '@fortawesome/free-brands-svg-icons';
+import './Footer.css'
 
 const Footer = () => {
-    return(
-        <div>
-            <footer className="footer text-white">
-                <div className="container">
-                    <footer className="py-5">
-                        <div className="row">
-                            <div className="col-3">
-                                <h4>Aysdev  Global Consultancy LLP</h4>
-                            </div>
-                            <div className="col-2">
-                                <h5>Section</h5>
-                                <ul className="nav flex-column">
-                                    <li className="nav-item mb-2">
-                                        <NavLink to="/" className="nav-link p-0 text-white">
-                                            Home
-                                        </NavLink>
-                                    </li>
-                                    <li className="nav-item mb-2">
-                                        <NavLink to="/services" className="nav-link p-0 text-white">
-                                            Services
-                                        </NavLink>
-                                    </li>
-                                    <li className="nav-item mb-2">
-                                        <NavLink to="/blog" className="nav-link p-0 text-white">
-                                            Blog
-                                        </NavLink>
-                                    </li>
-                                    {/* <li className="nav-item mb-2">
-                                        <NavLink to="#" className="nav-link p-0 text-white">
-                                            FAQs
-                                        </NavLink>
-                                    </li>
-                                    <li className="nav-item mb-2">
-                                        <NavLink to="/about" className="nav-link p-0 text-white">
-                                            About
-                                        </NavLink>
-                                    </li> */}
-                                </ul>
-                            </div> 
-                            <div className="col-2">
-                                <br />
-                                <br />
-                                {/* <h5>Section</h5> */}
-                                <ul className="nav flex-column">
-                                    {/* <li className="nav-item mb-2">
-                                        <NavLink to="/" className="nav-link p-0 text-white">
-                                            Home
-                                        </NavLink>
-                                    </li>
-                                    <li className="nav-item mb-2">
-                                        <NavLink to="/services" className="nav-link p-0 text-white">
-                                            Services
-                                        </NavLink>
-                                    </li>
-                                    <li className="nav-item mb-2">
-                                        <NavLink to="/blog" className="nav-link p-0 text-white">
-                                            Blog
-                                        </NavLink>
-                                    </li> */}
-                                    <li className="nav-item mb-2">
-                                        <NavLink to="/contact" className="nav-link p-0 text-white">
-                                            Contact Us
-                                        </NavLink>
-                                    </li>
-                                    <li className="nav-item mb-2">
-                                        <NavLink to="/about" className="nav-link p-0 text-white">
-                                            About
-                                        </NavLink>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div className="col-4 offset-1">
-                                <form>
-                                    <h5>Subscribe to our newsletter</h5>
-                                    <p>Monthly digest of whats new and exciting from us.</p>
-                                    <div className="d-flex w-100 gap-2">
-                                        <label htmlFor="newsletter1" className="visually-hidden">
-                                            Email address
-                                        </label>
-                                        <input
-                                        id="newsletter1"
-                                        type="text"
-                                        className="form-control"
-                                        placeholder="Email address"/>
-                                        <button className="btn btn-primary rounded-pill px-4"
-                                        type="button">Subscribe</button>
-                                    </div>
-                                </form>
-                            </div>
+  const [email, setEmail] = useState('');
 
-                        </div>
-                        <div className="d-flex justify-content-between pt-4 mt-4 border-top">
-                            <p>© 2021 Company, Inc. All rights reserved.</p>
-                            <ul className="list-unstyled d-flex">
-                                <li className="ms-3"><NavLink className="link-light" to="#"><i className="fa fa-facebook fa-2x"></i></NavLink></li>
-                                <li className="ms-3"><NavLink className="link-light" to="#"><i className="fa fa-instagram fa-2x"></i></NavLink></li>
-                                <li className="ms-3"><NavLink className="link-light" to="#"><i className="fa fa-twitter fa-2x"></i></NavLink></li>
-                            </ul>
-                        </div>
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
 
-                    </footer>
-                    
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform subscription logic with the email value
+    console.log('Subscribing email:', email);
+    setEmail('');
+  };
 
-                </div>
-            </footer>
-
-        </div>
-    );
-}
+  return React.createElement(
+    'footer',
+    { className: 'footer' },
+    React.createElement(
+      'div',
+      { className: 'footer-container' },
+      React.createElement(
+        'div',
+        { className: 'footer-column' },
+        React.createElement('h3', null, 'About'),
+        React.createElement(
+          'p',
+          null,
+          'Aysdev Global Consultancy provides industry-leading management consulting, and technology consulting  services to many of the world\'s most admired brands.'
+        )
+      ),
+      React.createElement(
+        'div',
+        { className: 'footer-column' },
+        React.createElement('h3', null, 'Services'),
+        React.createElement(
+          'ul',
+          null,
+          React.createElement(
+            'li',
+            null,
+            React.createElement('a', { href: '/', rel: 'noopener noreferrer' },
+             ' Management Consulting'
+            )
+          ),
+          React.createElement(
+            'li',
+            null,
+            React.createElement('a', { href: '/',  rel: 'noopener noreferrer' },
+              'Technology Management'
+            )
+          ),
+          
+          // React.createElement('a',{ href: '/', rel: 'noopener noreferrer' } , 'Management Consulting'),
+          // React.createElement('a', { href: '/', rel: 'noopener noreferrer' }, 'Technology Consulting'),
+        //   React.createElement('li', null, 'Tax'),
+        //   React.createElement('li', null, 'Advisory')
+        )
+      ),
+      React.createElement(
+        'div',
+        { className: 'footer-column' },
+        React.createElement('h3', null, 'Connect with us'),
+        React.createElement(
+          'ul',
+          { className: 'social-icons' },
+          React.createElement(
+            'li',
+            null,
+            React.createElement('a', { href: 'https://twitter.com/aysdev', target: '_blank', rel: 'noopener noreferrer' },
+              React.createElement(FontAwesomeIcon, { icon: faTwitter })
+            )
+          ),
+          React.createElement(
+            'li',
+            null,
+            React.createElement('a', { href: 'https://in.linkedin.com/company/aysdev-global-consultancy-llp?original_referer=https%3A%2F%2Fwww.google.com%2F', target: '_blank', rel: 'noopener noreferrer' },
+              React.createElement(FontAwesomeIcon, { icon: faLinkedin })
+            )
+          ),
+          React.createElement(
+            'li',
+            null,
+            React.createElement('a', { href: 'https://www.facebook.com/pages/Aysdev-Global-Consultancy-LLP/1988211104814110/', target: '_blank', rel: 'noopener noreferrer' },
+              React.createElement(FontAwesomeIcon, { icon: faFacebook })
+            )
+          )
+        )
+      ),
+      React.createElement(
+        'div',
+        { className: 'footer-column' },
+        React.createElement('h3', null, 'Subscribe to our Newsletter'),
+        React.createElement(
+          'form',
+          { onSubmit: handleSubmit },
+          React.createElement('input', { type: 'email', placeholder: 'Your email', value: email, onChange: handleEmailChange, required: true }),
+          React.createElement('button', { type: 'submit' }, 'Subscribe')
+        )
+      )
+    ),
+    React.createElement(
+      'div',
+      { className: 'footer-bottom' },
+      React.createElement('p', null, '\xA9 ', new Date().getFullYear(), ' Aysdev Global Consultancy LLP. All rights reserved.')
+    )
+  );
+};
 
 export default Footer;
