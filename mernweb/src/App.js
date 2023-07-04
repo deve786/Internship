853fd3756e9c6ed615a57268e5f9e3c8ce697639
Developer <ijasahmediku987@gmail.com>
2023-07-04 11:01:07 +0530

    AdminDash

--- mernweb/src/App.js
+++ mernweb/src/App.js
@@ -73,7 +73,7 @@ function App() {
           <Route path="user/profile" element={<Profile />} />
         </Route>
         <Route path="/dashboard" element={<AdminRoute />}>
-          {/* <Route path="admin" element={<AdminDashboard />} /> */}
+          <Route path="admin" element={<AdminDashboard />} />
           <Route path="admin/create-category" element={<CreateCategory />} />
           <Route path="admin/create-product" element={<CreateProduct />} />
           <Route path="admin/product/:slug" element={<UpdateProduct />} />
