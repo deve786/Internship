b75c215821b381d245121963932b74baeb297cdb
Developer <ijasahmediku987@gmail.com>
2023-07-05 12:00:37 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -26,7 +26,9 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://vercel.com/deve786/internship-zorc'
+}));
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
